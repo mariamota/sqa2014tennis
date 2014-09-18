e7eddf18d4118314857b67e597f2941823be7ab2
maria juana mota <juanis315@hotmail.com>
2014-09-18 15:18:21 -0500

    cambios
    escenarios

--- app/match.py
+++ app/match.py
@@ -1,6 +1,51 @@
 # -*- coding: utf-8 -*-
 class Match:
-    def __init__(self,player1,player2,pacted_sets):
+
+    def __init__(self, player1, player2, pacted_sets):
         self.p1 = player1
         self.p2 = player2
-        self.pacted_sets = pacted_sets
+        self.won = [0, 0]
+        self.sets = pacted_sets
+        self.set = ""
+        self.c = 0
+        self.d = [0, 0]
+        self.s1 = [0, 0, 0, 0, 0]
+        self.s2 = [0, 0, 0, 0, 0]
+
+    def score(self):
+        return "{0} plays with {1} | 0-0".format(self.p1, self.p2)
+
+    def won_set(self, w, s, s1, s2):
+        self.set = s
+        if(self.c < self.sets):
+            if(self.p1 == w):
+                self.won[0] = self.won[0] + 1
+                self.s1[self.c] = s1
+                self.s2[self.c] = s2
+                self.d[0] = int(s1) - int(s2)
+            if(self.p2 == w):
+                self.won[1] = self.won[1] + 1
+                self.s1[self.c] = s2
+                self.s2[self.c] = s1
+                self.d[1] = int(s2) - int(s1)
+            self.c += 1
+        else:
+            self.resultados(self)
+
+    def resultados(self):
+        if(self.won[1] < self.won[0]):
+            return "{0} defeated {1} | {2}".format(self.p1, self.p2, self.imprimir(self.p1))
+        elif(self.won[0] < self.won[1]):
+            return "{0} defeated {1} | {2}".format(self.p2, self.p1, self.imprimir(self.p2))
+
+    def imprimir(self, w):
+        sal = ""
+        x = 0
+        for x in range(self.c):
+            if(w == self.p1):
+                sal += str(self.s1[x]) + "-" + str(self.s2[x])
+            else:
+                sal += str(self.s2[x]) + "-" + str(self.s1[x])
+            if(x < self.c - 1):
+                sal += ", "
+        return sal
--- tests/features/steps.py
+++ tests/features/steps.py
@@ -3,33 +3,28 @@ from lettuce import *
 import app.match as m
 
 
-@step(u'Given: "([^"]*)" and "([^"]*)" start a match to "([^"]*)" sets')
-def given_group1_and_group2_start_a_match_to_group3_sets(step, group1, group2, group3):
-    world.match = m.Match(group1,group2,group3)
+@step('Given: "([^"]*)" and "([^"]*)" start a match to "([^"]*)" sets')
+def given_group1_and_group2_start_a_match_to_group3_sets(step, p1, p2, s):
+    world.match = m.Match(p1, p2, s)
 
 
-@step(u'When: "([^"]*)" and "([^"]*)" start a match to "([^"]*)" sets')
-def when_group1_and_group2_start_a_match_to_group3_sets(step, group1, group2, group3):
-    assert False, 'This step must be implemented'
+@step('Then: I see score: "([^"]*)"')
+def then_i_see_score_group1(step, set):
+    assert world.match.score() == set, \
+        "Got %s" % world.match.score()
 
 
-@step(u'Then: I see score: "([^"]*)"')
-def then_i_see_score_group1(step, group1):
-    assert False, 'This step must be implemented'
+@step('When: "([^"]*)" won the "([^"]*)" set "([^"]*)"-"([^"]*)"')
+def when_group1_won_the_group2_set_group3_group4(step, w, s, s1, s2):
+    world.match.won_set(w, s, s1, s2)
 
 
-@step(u'When: "([^"]*)" won the "([^"]*)" set "([^"]*)"-"([^"]*)"')
-def when_group1_won_the_group2_set_group3_group4(step, group1, group2, group3, group4):
-    assert False, 'This step must be implemented'
+@step('And: "([^"]*)" won the "([^"]*)" set "([^"]*)"-"([^"]*)"')
+def and_group1_won_the_group2_set_group3_group4(step, w, s, s1, s2):
+    world.match.won_set(w, s, s1, s2)
 
 
-@step(u'And: "([^"]*)" won the "([^"]*)" set "([^"]*)"-"([^"]*)"')
-def and_group1_won_the_group2_set_group3_group4(step, group1, group2, group3, group4):
-    assert False, 'This step must be implemented'
-
-
-@step(u'Then: The match score is: "([^"]*)"')
-def then_the_match_score_is_group1(step, group1):
-    assert False, 'This step must be implemented'
-
-
+@step('Then: The match score is: "([^"]*)"')
+def then_the_match_score_is_group1(step, res):
+    assert world.match.resultados() == res, \
+        "Got %s" % world.match.resultados()
